9b0120c16130d3df1375e6f91eb1231c204666fc
laloxox <lalopx555@gmail.com>
2025-03-06 17:32:19 -0600

    Diseño de componentes de la pagina

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    darkMode: '',
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
